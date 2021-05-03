3672001aa62c5caf43c5a1ce80238c72625d6bde
CaseyDelaney <51470438+CaseyDelaney@users.noreply.github.com>
2021-05-03 13:15:40 -0700

    Create TestData.sql

---
+++ Code/SQL_Queries/TestData.sql
@@ -0,0 +1,6 @@
+INSERT employee VALUES 
+('123', 'Alice', '333 Dorsey Lane','MON-FRI (9am-2pm)', 'herPass'),
+('234', 'Bob', '444 Rain Rd','MON-FRI (2PM-10PM)', 'hisPass'),
+('392', 'Vero', '355 Burrow Rd','MON-FRI (2PM-10PM)', 'verito'),
+('899', 'Rythor', '277 SJ street','MON-FRI (4PM-10PM)', 'ryan82'),
+('589', 'Carther', '4822 Maple Ct','MON-FRI (9AM-5pm)', 'canadianSyrup');
